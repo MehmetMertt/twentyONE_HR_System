103f70009fb36dec4c67762cf24efb2cf831bb7d
MartinsGIT1 <145049757+MartinsGIT1@users.noreply.github.com>
2024-05-05 01:32:44 +0200

    Added Password encyrption

--- HR_Managment_System_Application/dbmanager.cpp
+++ HR_Managment_System_Application/dbmanager.cpp
@@ -1,5 +1,6 @@
 #include "dbmanager.h"
-
+#include "simplecrypt.h"
+ #include <QCryptographicHash>
 //Konstruktur der dbmanager klasse, ermögicht einssen zentralisierten Zugriff auf die DB
 dbmanager::dbmanager(const QString path) {
     m_db = QSqlDatabase::addDatabase("QMYSQL");
@@ -40,14 +41,21 @@ bool dbmanager::login(QString email, QString passwort){
 bool dbmanager::addMitarbeiter(QString vorname, QString nachname, QString email, QString telenr,QString passwort){
     bool success = false;
     QSqlQuery query;
+
+
+
+    QByteArray passwortHash = passwort.toUtf8(); //verwandelt Qsrting passwort in Qbyte array
+    passwortHash=QCryptographicHash::hash(passwortHash,QCryptographicHash::Sha3_512); //Qbyte array wird geshasht mit Sha512 hash
+    QString passwortHashAsString(passwortHash); //Qbyte array wird zurueck in string verwandelt
+
+
+
     query.prepare("INSERT INTO Mitarbeiter (Vorname, Nachname, Email, Telefonnummer, Passwort) VALUES(:Vorname, :Nachname, :Email, :Telefonnummer,:Passwort);");
     query.bindValue(":Vorname",QString("'%1'").arg(vorname));
     query.bindValue(":Nachname",QString("'%1'").arg(nachname));
     query.bindValue(":Email",QString("'%1'").arg(email));
     query.bindValue(":Telefonnummer",QString("'%1'").arg(telenr));
-    query.bindValue(":Passwort",QString("'%1'").arg(passwort));
-
-
+    query.bindValue(":Passwort",QString("'%1'").arg(passwortHashAsString));
     qDebug() << query.lastQuery();
 
     if(query.exec())
