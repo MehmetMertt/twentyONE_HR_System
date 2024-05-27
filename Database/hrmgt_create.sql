4deb88fb68a478cfd3081500bdaeae7be274f7f2
Mehmet M. <146014978+MehmetMertt@users.noreply.github.com>
2024-05-27 16:35:23 +0200

    SQL Script

--- Database/hrmgt_create.sql
+++ Database/hrmgt_create.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Erstellungszeit: 15. Mai 2024 um 19:07
+-- Erstellungszeit: 27. Mai 2024 um 14:34
 -- Server-Version: 8.4.0
 -- PHP-Version: 8.2.8
 
@@ -20,11 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `hrmgt_database`
 --
-
-DROP DATABASE IF EXISTS hrmgt_database;
-CREATE DATABASE hrmgt_database;
-
-use hrmgt_database;
 
 -- --------------------------------------------------------
 
@@ -92,15 +87,16 @@ CREATE TABLE `EMPLOYEE` (
   `adressid` int DEFAULT NULL,
   `departmentid` int DEFAULT NULL,
   `salaryid` int DEFAULT NULL,
-  `admin` int DEFAULT 0
+  `admin` int DEFAULT '0',
+  `title` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Daten für Tabelle `EMPLOYEE`
 --
 
-INSERT INTO `EMPLOYEE` (`id`, `password`, `name`, `surname`, `mail`, `phone`, `employmentdate`, `terminationdate`, `adressid`, `departmentid`, `salaryid`) VALUES
-(31, 'd787a3c0317fd70de01da92b71d1808bf3d4ae75ff6693dae9289b5fe9997d24bf2f1d4810526b9f12ea38ffe7fd526b53811bf8b9df567c2ac9fa177a09b0d8', 'Flo', 'Mimmler', 'fmimmler@gmail.com', '+43 67006070522', NULL, NULL, 1, NULL, NULL);
+INSERT INTO `EMPLOYEE` (`id`, `password`, `name`, `surname`, `mail`, `phone`, `employmentdate`, `terminationdate`, `adressid`, `departmentid`, `salaryid`, `admin`, `title`) VALUES
+(31, 'd787a3c0317fd70de01da92b71d1808bf3d4ae75ff6693dae9289b5fe9997d24bf2f1d4810526b9f12ea38ffe7fd526b53811bf8b9df567c2ac9fa177a09b0d8', 'Flo', 'Mimmler', 'fmimmler@gmail.com', '+43 67006070522', NULL, NULL, 1, NULL, NULL, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -112,6 +108,26 @@ CREATE TABLE `SALARY` (
   `id` int NOT NULL,
   `salary` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `TITLES`
+--
+
+CREATE TABLE `TITLES` (
+  `id` int NOT NULL,
+  `title` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Daten für Tabelle `TITLES`
+--
+
+INSERT INTO `TITLES` (`id`, `title`) VALUES
+(1, 'Herr'),
+(2, 'Frau'),
+(3, 'Divers');
 
 -- --------------------------------------------------------
 
@@ -158,12 +174,19 @@ ALTER TABLE `EMPLOYEE`
   ADD UNIQUE KEY `mail` (`mail`),
   ADD KEY `OrtID` (`adressid`),
   ADD KEY `AbteilungsID` (`departmentid`),
-  ADD KEY `GehaltID` (`salaryid`);
+  ADD KEY `GehaltID` (`salaryid`),
+  ADD KEY `title` (`title`);
 
 --
 -- Indizes für die Tabelle `SALARY`
 --
 ALTER TABLE `SALARY`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `TITLES`
+--
+ALTER TABLE `TITLES`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -199,13 +222,19 @@ ALTER TABLE `DEPARTMENT`
 -- AUTO_INCREMENT für Tabelle `EMPLOYEE`
 --
 ALTER TABLE `EMPLOYEE`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT für Tabelle `SALARY`
 --
 ALTER TABLE `SALARY`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `TITLES`
+--
+ALTER TABLE `TITLES`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT für Tabelle `WORKINGHOURS`
@@ -229,7 +258,8 @@ ALTER TABLE `ABSENCE`
 ALTER TABLE `EMPLOYEE`
   ADD CONSTRAINT `EMPLOYEE_ibfk_1` FOREIGN KEY (`adressid`) REFERENCES `ADDRESS` (`id`),
   ADD CONSTRAINT `EMPLOYEE_ibfk_2` FOREIGN KEY (`departmentid`) REFERENCES `DEPARTMENT` (`id`),
-  ADD CONSTRAINT `EMPLOYEE_ibfk_3` FOREIGN KEY (`salaryid`) REFERENCES `SALARY` (`id`);
+  ADD CONSTRAINT `EMPLOYEE_ibfk_3` FOREIGN KEY (`salaryid`) REFERENCES `SALARY` (`id`),
+  ADD CONSTRAINT `EMPLOYEE_ibfk_4` FOREIGN KEY (`title`) REFERENCES `TITLES` (`id`);
 
 --
 -- Constraints der Tabelle `WORKINGHOURS`
